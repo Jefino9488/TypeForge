f573aba841e66ab07cb185c759389b71875a1a01
Jefino9488 <jefinojacob9488@gmail.com>
2026-07-04 00:36:14 +0530

    chore: configure gitignore and add release workflow

--- build/CMakeFiles/typeforge-im.conf.in-fmt.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/typeforge-im.conf.in-fmt"
-  "typeforge-im.conf"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/typeforge-im.conf.in-fmt.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/CMakeFiles/typeforge.conf.in-fmt.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/typeforge.conf.in-fmt"
-  "typeforge.conf"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/typeforge.conf.in-fmt.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
